407eb63074aff8482d11077434b3b299c234d2e7
FerryFahrenkrog <30522314+FerryFahrenkrog@users.noreply.github.com>
2021-12-15 12:57:00 +0100

    VK_4_Ende

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
@@ -1,25 +1,53 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class DetailviewActivity extends AppCompatActivity {
 
     public static final String ARG_ITEM = "item";
     private EditText itemNameText;
     private String item;
+    private FloatingActionButton saveButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //Zustand wiedeherstellen wenn man das Handy dreht
 
         setContentView(R.layout.activity_detailview);
-        itemNameText = findViewById(R.id.itemName);
 
+        //1. Bedien- und Anzeigeelemente auslesen
+        itemNameText = findViewById(R.id.itemName);
+        saveButton = findViewById(R.id.saveButton);     // evtl oben refactoren?
+
+        //2. Bedienelemente bedienbar machen
+        saveButton.setOnClickListener(v -> onSaveItem());
+
+        //3. Ansicht mit Daten befüllen
         item = getIntent().getStringExtra(ARG_ITEM);
-        itemNameText.setText(item); //Data Binding
+
+        if(item != null){
+            itemNameText.setText(item); //Data Binding
+        }
+
+    }
+
+    protected void onSaveItem(){
+        String itemName = itemNameText.getText().toString(); //toString testen?
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(ARG_ITEM, itemName);
+
+        setResult(Activity.RESULT_OK, returnIntent);
+
+        finish();
+
     }
 }
--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
@@ -1,8 +1,10 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.dieschnittstelle.mobile.android.skeleton.R;
@@ -19,7 +22,11 @@ import java.time.LocalDateTime;
 
 public class MainActivity extends AppCompatActivity
 {
-    private ViewGroup listView;     // Allgemeine Gruppe 32:32Min
+    //Bedienelemente
+    private ViewGroup listView;
+    private FloatingActionButton addNewItemButton;
+
+    private static final int CALL_DETAILVIEW_FOR_CREATE = 0;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
+        addNewItemButton = findViewById(R.id.addNewItemButton);
 
         for(int i=0; i<listView.getChildCount(); i++){
             TextView currentChild = (TextView) listView.getChildAt(i);
@@ -35,6 +43,7 @@ public class MainActivity extends AppCompatActivity
                 onItemSelected(currentChild.getText().toString());
             });
         }
+        addNewItemButton.setOnClickListener(v -> this.onItemCreationRequested());
     }
 
     protected void onItemSelected(String itemName){
@@ -42,8 +51,35 @@ public class MainActivity extends AppCompatActivity
         detailviewIntent.putExtra(DetailviewActivity.ARG_ITEM, itemName); // in Arg Item das zweite reinpacken
         this.startActivity(detailviewIntent);
     }
-//
-//    protected void showFeedbackMessage(String msg){
-//        Snackbar.make(findViewById(R.id.rootView), msg, Snackbar.LENGTH_INDEFINITE).show();
-//    }
+
+    protected void onItemCreationRequested(){
+        Intent detailviewForCreateIntent = new Intent(this, DetailviewActivity.class);
+        startActivityForResult(detailviewForCreateIntent, CALL_DETAILVIEW_FOR_CREATE);
+    }
+
+    //1h 17min DRIN LASSEN
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CALL_DETAILVIEW_FOR_CREATE){
+            if(resultCode == Activity.RESULT_OK)    // Sagt aus ob man einfach nur zurückgegangen ist oder was durchgeführt hat
+            {
+                onNewItemCreated(data.getStringExtra(DetailviewActivity.ARG_ITEM));  //1h 28 min!!!!!!!!!!!!!
+            }
+            else{
+                showFeedbackMessage("Returning from detailview with " + resultCode);
+            }
+        }
+    }
+
+    protected void showFeedbackMessage(String msg){
+        Snackbar.make(findViewById(R.id.rootView), msg, Snackbar.LENGTH_SHORT).show();
+    }
+
+    protected void onNewItemCreated(String itemName){
+//        showFeedbackMessage("created new item " + itemName);
+        TextView newItemView = (TextView) getLayoutInflater().inflate(R.layout.activity_main_listitem, null);
+        newItemView.setText(itemName);
+        listView.addView(newItemView);
+    }
 }
