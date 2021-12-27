63ac431d5f118fa28b10501e1daf0fba4347dc87
FerryFahrenkrog <30522314+FerryFahrenkrog@users.noreply.github.com>
2021-12-27 08:45:28 +0100

    Zwischenstand Vk 10

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -14,6 +18,8 @@ import org.dieschnittstelle.mobile.android.skeleton.databinding.ActivityDetailvi
 import model.ToDo;
 
 public class DetailviewActivity extends AppCompatActivity {
+
+    public static final int ACTION_PICK_CONTACT = 0;
 
     public static final String ARG_ITEM = "item";
 
@@ -42,7 +48,6 @@ public class DetailviewActivity extends AppCompatActivity {
         setResult(Activity.RESULT_OK, returnIntent);
 
         finish();
-//        showContacts();
     }
 
     public ToDo getItem() {
@@ -53,9 +58,36 @@ public class DetailviewActivity extends AppCompatActivity {
         this.item = item;
     }
 
-    public void showContacts()
-    {
-        Intent contactSelectionIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(contactSelectionIntent, 0);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.detailview_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(item.getItemId() == R.id.selectContact){
+            selectContact();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void selectContact(){
+        Intent selectContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(selectContactIntent,ACTION_PICK_CONTACT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == ACTION_PICK_CONTACT && resultCode == Activity.RESULT_OK)
+        {
+            Log.i("DetailviewActivity", "onActivityResult(): got data: " + data);
+        }
+        else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    //1h 15 REST
 }
